ae9dea5075d1a2f0735eb57916e77b30b9a454b6
alarc <alarc@DESKTOP-HU9ROBJ>
2022-06-16 20:12:43 -0500

    cambios

---
+++ src/java/upeu/entity/Vendedor.java
@@ -0,0 +1,17 @@
+
+package upeu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Vendedor {
+ private int idvendedor;
+ private String nombre;
+ private int idpersona;
+}
